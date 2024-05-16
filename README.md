8a2807f9db469cc86134af4918481c3d6a37a4e8
MK <koutny.marian@seznam.cz>
2024-05-16 16:17:04 +0200

    Update project questions and word document

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
-# sql-repo
-# testovani
-# pokusny kralik
-# Hrani si se zakladnimi tabulkovymi sety
-# Update v kreaci tabulek
-# 21/04/2024 Utvareni zakladni podoby nekterych tabulek a popisku - finish row 160 cca
-# Zbytek času kódování úkolů, změny, také zkoušení si, jak se script chová, když měním parametry apod.
+Projekt pro výzkumné oddělení 
---
+++ SQL_project_Marian_Koutny_tables.sql
@@ -0,0 +1,172 @@
+/*
+ SQL PROJECT - ENGETO DATOVÁ AKADEMIE (START 22/02/2024) - MARIAN KOUTNÝ
+ */
+
+-- ČÁST PRVNÍ - VYTVOŘENÍ DVOU FINÁLNÍCH TABULEK K PROJEKTU
+
+/*
+1. Výpis datových setů potřebných k projektu
+ */
+SELECT * FROM czechia_region cr;
+SELECT * FROM czechia_district cd;		
+SELECT * FROM czechia_price_category cpc ORDER BY name ASC;
+SELECT * FROM czechia_price cp;
+SELECT * FROM czechia_price cp;
+SELECT * FROM czechia_payroll cp;
+SELECT * FROM czechia_payroll_industry_branch cpib;
+SELECT * FROM czechia_payroll_calculation cpc; 
+SELECT * FROM czechia_payroll_unit cpu;
+SELECT * FROM czechia_payroll_value_type cpvt;
+SELECT * FROM countries c;
+SELECT * FROM economies e;
+
+/*
+2. Vytvoření pomocných tabulek, pomoci kterých se dostaneme k první finální tabulce
+ */
+
+
+/*
+2a) Vývoj průměrných platů v jednotlivých odvětvích mezi lety 2000 a 2021 - pomocná tabulka t_mk_wage
+ */
+
+CREATE OR REPLACE TABLE t_mk_wage AS (
+SELECT
+	cpib.name AS branch,
+	cp.payroll_year,
+	round(sum (value)/count (payroll_year),0) AS avg_wage_per_branch
+FROM czechia_payroll cp
+LEFT JOIN czechia_payroll_industry_branch cpib
+ON cp.industry_branch_code = cpib.code
+WHERE value_type_code = 5958 AND industry_branch_code IS NOT NULL
+GROUP BY cpib.name, cp.payroll_year
+);
+
+SELECT * FROM t_mk_wage tmw;
+
+
+/*
+2b) Vývoj průměrných cen jednotlivých potravin v letech 2006 až 2018 v daných krajích - tabulka t_mk_price
+ */
+
+CREATE OR REPLACE TABLE t_mk_price AS (
+SELECT 
+	cpc.name AS food,
+	year(cp.date_from) AS `year`,
+	cr.name AS region,
+	round(sum(cp.value)/count(YEAR(cp.date_from)),2) AS avg_price
+FROM czechia_price cp
+LEFT JOIN czechia_price_category cpc
+	ON cp.category_code = cpc.code
+LEFT JOIN czechia_region cr
+	ON cp.region_code = cr.code
+WHERE cr.name IS NOT NULL
+GROUP BY cpc.name, cr.name, YEAR(cp.date_from)
+ORDER BY `year`, food
+);
+ 
+SELECT * FROM t_mk_price tmp;
+
+
+/*
+2c) Vývoj průměrných cen potravin v letech 2006 až 2018 (průměr za všechny kraje) - pomocná tabulka t_mk_price_general
+ */
+
+CREATE OR REPLACE TABLE t_mk_price_general AS (
+SELECT 
+	tmp.food AS foodstuff,
+	tmp.`year` AS `year`,
+	round(sum(tmp.avg_price)/count(tmp.`year`),2) AS avg_price_year
+FROM t_mk_price tmp
+WHERE tmp.region IS NOT NULL
+GROUP BY tmp.food, tmp.`year`
+ORDER BY tmp.`year`, tmp.food
+);
+ 
+
+SELECT * FROM t_mk_wage tmw;
+SELECT * FROM t_mk_price_general tmg;
+
+
+/*
+3. Vytvoření první finální tabulky t_Marian_Koutny_project_SQL_primary_final:
+ */
+
+CREATE OR REPLACE TABLE t_Marian_Koutny_project_SQL_primary_final AS (
+SELECT
+	tmw.branch,
+	tmw.payroll_year,
+	tmw.avg_wage_per_branch,
+	tmg.foodstuff,
+	tmg.avg_price_year
+FROM t_mk_wage tmw
+LEFT JOIN t_mk_price_general tmg ON tmw.payroll_year = tmg.`year`
+);
+
+/*
+3a) Modifikace sloupce a vytvoření indexu
+ */
+
+ALTER TABLE t_marian_koutny_project_sql_primary_final MODIFY COLUMN branch varchar(70);
+CREATE OR REPLACE INDEX i_tm_branch ON t_marian_koutny_project_sql_primary_final(branch);
+
+
+/*
+4. Vytvoření pomocné tabulky pro sekundární tabulku projektu - tabulka t_ec
+ */
+
+CREATE OR REPLACE TABLE t_ec AS (
+SELECT 
+	e.country,
+	e.`year`,
+	e.GDP,
+	e.gini,
+	e.population,
+	c.continent
+FROM economies e
+JOIN countries c ON e.country = c.country 
+WHERE c.government_type NOT LIKE '%Territory%' and c.government_type NOT LIKE '%of%'
+and c.government_type NOT LIKE '%administrated%'and c.government_type NOT LIKE '%occupied%'
+AND e.GDP IS NOT NULL  AND e.`year` BETWEEN 2000 AND 2021
+ORDER BY e.country ASC, e.`year` DESC 
+);
+
+SELECT * FROM t_ec te;
+
+
+/*
+5. Vytvoření druhé finální tabulky t_marian_koutny_project_sql_secondary_final
+ */
+
+CREATE OR REPLACE TABLE t_marian_koutny_project_sql_secondary_final AS (
+SELECT 
+	te.country, 
+	round(te.GDP,0) AS GDP,
+	te.YEAR AS cur_year, 
+	te2.YEAR as year_prev,
+	round( ( te.GDP - te2.GDP ) / te2.GDP * 100, 2 ) as GDP_growth,
+	te.population AS population_cur_y,
+	te.gini
+FROM t_ec te 
+JOIN t_ec te2 
+    ON te.country = te2.country 
+    AND te.YEAR - 1 = te2.YEAR
+    AND te.year <= 2021
+WHERE te.continent = 'Europe'
+);
+
+
+/*
+5a) DROP již nepotřebných pomocných tabulek
+ */
+DROP TABLE t_mk_price_general;
+DROP TABLE t_mk_price;
+DROP TABLE t_mk_wage;
+DROP TABLE t_ec;
+
+
+/* 
+5b) Náhled do obou finálních tabulek
+ */
+
+SELECT * FROM t_marian_koutny_project_sql_primary_final tm;
+SELECT * FROM t_marian_koutny_project_sql_secondary_final ts;
